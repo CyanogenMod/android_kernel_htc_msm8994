f22f785ce8b7aefda35b8d5c10e6a15a457370c0
Ingrid Gallardo <ingridg@codeaurora.org>
2015-04-07 20:09:21 -0700

    msm: mdss: add support to set the minimum mdp transfer time
    For command mode panels current code calculations adjust the mdp
    clock rate based in the minimum dsi pixel clock rate. This is
    wrong and can lead to power issues since this rate considers
    the blanking times which are not required for the mdp clock
    calculations.
    This change optimize the calculations by only considering the worst
    width that could be processed by the mdp without blanking periods
    and consider the time expected for the transfer by adjusting
    the mdp clock in order to meet the performance requested; this
    time can be tuned through the panel configuration.
    
    Conflicts:
    	drivers/video/msm/mdss/mdss_dsi_panel.c
    
    Change-Id: Ib25df0e8f749d3ffc1d9059d5d7eac27e0f0b81e
    Signed-off-by: Ingrid Gallardo <ingridg@codeaurora.org>
    Signed-off-by: Chandan Uddaraju <chandanu@codeaurora.org>

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -29,6 +29,7 @@
 
 #define DT_CMD_HDR 6
 #define MIN_REFRESH_RATE 30
+#define DEFAULT_MDP_TRANSFER_TIME 14000
 
 #define BRI_SETTING_DEF 142
 
@@ -1823,6 +1824,8 @@ static int mdss_panel_parse_dt(struct device_node *np,
 	pinfo->mipi.frame_rate = (!rc ? tmp : 60);
 	rc = of_property_read_u32(np, "qcom,mdss-dsi-panel-clockrate", &tmp);
 	pinfo->clk_rate = (!rc ? tmp : 0);
+	rc = of_property_read_u32(np, "qcom,mdss-mdp-transfer-time-us", &tmp);
+	pinfo->mdp_transfer_time_us = (!rc ? tmp : DEFAULT_MDP_TRANSFER_TIME);
 	data = of_get_property(np, "qcom,mdss-dsi-panel-timings", &len);
 	if ((!data) || (len != 12)) {
 		pr_err("%s:%d, Unable to read Phy timing settings",
