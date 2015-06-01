6553f0dcaae7235992288b2686bd5ca2818116fe
Lingutla Chandrasekhar <clingutla@codeaurora.org>
2015-06-01 17:16:25 +0530

    soc: qcom: memory_dump_v2: use kmemleak_not_leak for mem dump's app table
    table (of type msm_dump_table) is virtual address for app's base
    table of memdump, which is converted to physical address and
    holds it in the memdump.table_phys. This memory is not intended
    to be freed. When kmemleak scans, it can't deduce that the reference
    we are holding is via physical address and not virtual address.
    Hence, kmemleak will treat this as a leak as it can't find
    a reference to this variable. Supress this by using kmemleak_not_leak.
    
    Change-Id: I5236c940688829a3b5f255c5ee9fcf309cf8ebb7
    Signed-off-by: Lingutla Chandrasekhar <clingutla@codeaurora.org>

--- drivers/soc/qcom/memory_dump_v2.c
+++ drivers/soc/qcom/memory_dump_v2.c
@@ -17,6 +17,7 @@
 #include <linux/err.h>
 #include <linux/of.h>
 #include <linux/of_address.h>
+#include <linux/kmemleak.h>
 #include <soc/qcom/memory_dump.h>
 #include <soc/qcom/scm.h>
 
@@ -200,6 +201,7 @@ static int __init init_memory_dump(void)
 		goto err1;
 	}
 #endif
+	kmemleak_not_leak(table);
 	table->version = MSM_DUMP_TABLE_VERSION;
 
 	entry.id = MSM_DUMP_TABLE_APPS;
