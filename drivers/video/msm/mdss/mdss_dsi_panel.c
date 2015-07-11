e72c713450a803362751ac7d12d8029425da277d
Rashed Abdel-Tawab <rashed@linux.com>
2015-07-11 19:56:44 -0400

    msm: mdss: Don't log brightness changes

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -189,7 +189,7 @@ static unsigned int bl_to_brightness(int val, int brt_dim, int brt_min, int brt_
 	} else if (val > BRI_SETTING_MAX)
 		brt_val = brt_max;
 
-	PR_DISP_INFO("%s:level=%d, brightness=%d", __func__, val, brt_val);
+	//PR_DISP_INFO("%s:level=%d, brightness=%d", __func__, val, brt_val);
 	return brt_val;
 }
 #if 0
@@ -588,7 +588,7 @@ static void mdss_dsi_panel_bl_ctrl(struct mdss_panel_data *pdata,
 							ctrl_pdata->brt_extra,
 							ctrl_pdata->brt_max);
 
-		PR_DISP_INFO("%s:bl_level=%d\n", __func__, bl_level);
+		//PR_DISP_INFO("%s:bl_level=%d\n", __func__, bl_level);
 		led_trigger_event(bl_led_trigger, bl_level);
 		break;
 	case BL_PWM:
