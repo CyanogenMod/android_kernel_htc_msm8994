95e29a27edc2c778ce4c56f2864cba7f9d89d9c1
Rashed Abdel-Tawab <rashed@linux.com>
2015-10-11 13:00:59 -0400

    ASoC: msm: qdsp6v2: Remove rogue config_effects
    Change-Id: I210bf7766b845705006c73b65e53df85fc905456

--- sound/soc/soc-compress.c
+++ sound/soc/soc-compress.c
@@ -739,7 +739,6 @@ static struct snd_compr_ops soc_compr_ops = {
 	.ack			= soc_compr_ack,
 	.get_caps		= soc_compr_get_caps,
 	.get_codec_caps		= soc_compr_get_codec_caps
-	.config_effect 		= soc_compr_config_effect 
 };
 
 static struct snd_compr_ops soc_compr_dyn_ops = {
@@ -755,7 +754,6 @@ static struct snd_compr_ops soc_compr_dyn_ops = {
 	.ack			= soc_compr_ack,
 	.get_caps		= soc_compr_get_caps,
 	.get_codec_caps		= soc_compr_get_codec_caps
-	.config_effect 		= soc_compr_config_effect 
 };
 
 int soc_new_compress(struct snd_soc_pcm_runtime *rtd, int num)
