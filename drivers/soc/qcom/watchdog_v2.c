264d4fa40fc3425d0fdfd0a791493ce74b203a20
Ethan Chen <intervigil@gmail.com>
2015-12-14 15:45:06 -0800

    soc: qcom: watchdog_v2: Fix pointer passed to PM functions
    * HTC passed an iomem here, instead of the device data, this leads the a
      crazy kernel panic.
    
    Change-Id: I0f8ce2c2efc459c9cba274207e8a3392032ff410

--- drivers/soc/qcom/watchdog_v2.c
+++ drivers/soc/qcom/watchdog_v2.c
@@ -188,7 +188,7 @@ static int msm_watchdog_suspend(struct device *dev)
 	}
 #endif
 
-	msm_watchdog_do_suspend(wdog_dd->base);
+	msm_watchdog_do_suspend(wdog_dd);
 
 	return 0;
 }
@@ -224,7 +224,7 @@ static int msm_watchdog_resume(struct device *dev)
 	}
 #endif
 
-	msm_watchdog_do_resume(wdog_dd->base);
+	msm_watchdog_do_resume(wdog_dd);
 
 	return 0;
 }
