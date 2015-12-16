4e3fe5f95735588234746deb4846df42652d1c26
Ethan Chen <intervigil@gmail.com>
2015-12-16 10:11:46 -0800

    input: touchscreen: max1187x: Allow touchscreen use in recovery
    Change-Id: I81f47c1c9e1db2a7b72a680698a45abc0f395329

--- drivers/input/touchscreen/max1187x.c
+++ drivers/input/touchscreen/max1187x.c
@@ -4254,8 +4254,7 @@ static int probe(struct i2c_client *client, const struct i2c_device_id *id)
 		goto err_chip_exist;
 	}
 
-	if ((strcmp(htc_get_bootmode(), "offmode_charging") == 0)
-		|| (strcmp(htc_get_bootmode(), "recovery") == 0)) {
+	if (strcmp(htc_get_bootmode(), "offmode_charging") == 0) {
 		pr_info("(PROBE): %s mode", htc_get_bootmode());
 		off_mode_suspend(client);
 		goto err_off_mode;
